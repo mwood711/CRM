e7762fa2220bf8f4389d8031bddae9518a22e1e9
Randy Akuffo <randyakuffo91@gmail.com>
2020-02-21 20:06:45 -0500

    testing

---
+++ AddEvent.php
@@ -0,0 +1,32 @@
+<?php
+$evt_name = $_POST['evt_name'];
+$evi_idd = $_POST['evi_idd'];
+if ( !empty($evt_name) || !empty($evi_idd)) {
+    $serverName = "localhost\\SQLEXPRESS";
+ //creating a connection to the SQL database
+    $connectionInfo = array( "Database"=>"ksu_crm");
+    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+    if( $conn ) 
+    {
+        echo "Connection established.<br />";
+   }
+   else{
+        echo "Connection could not be established.<br />";
+        die( print_r( sqlsrv_errors(), true));
+    } 
+//Insert data into the database
+    {
+     $INSERT = "INSERT Into eventtype (evt_name, evi_idd) VALUES('$evt_name', '$evi_idd' )";
+//Prepare statement
+      $stmt = sqlsrv_prepare($conn, $INSERT);
+      sqlsrv_execute($stmt);
+      echo "New record inserted sucessfully";
+     } 
+     sqlsrv_close($conn);
+
+    }
+ else {
+ echo "All field are required";
+ die();
+}
+?>
