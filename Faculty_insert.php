063cc74c5d507108e097a9c5d2b6e88c4c347b10
Randy Akuffo <randyakuffo91@gmail.com>
2020-02-24 09:52:50 -0500

    Renameed Files for the appropriate name

--- Faculty_insert.php
+++ Faculty_insert.php
@@ -1,11 +1,11 @@
 <?php
 $f_id = $_POST['f_id'];
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $address = $_POST['address'];
-if ( !empty($f_id) || !empty($fname) || !empty($lname) || !empty($email) || !empty($phone) || !empty($address)) {
+if ( !empty($f_id) || !empty($firstname) || !empty($lastname) || !empty($email) || !empty($phone) || !empty($address)) {
     $serverName = "localhost\\SQLEXPRESS";
  //creating a connection to the SQL database
     $connectionInfo = array( "Database"=>"ksu_crm");
@@ -20,7 +20,7 @@ if ( !empty($f_id) || !empty($fname) || !empty($lname) || !empty($email) || !emp
     } 
 //Insert data into the database
     {
-     $INSERT = "INSERT Into faculty (f_id, fname, lname, email, phone, address) VALUES('$f_id', '$fname', '$lname', '$email', '$phone', '$address')";
+     $INSERT = "INSERT Into faculty (f_id, firstname, lastname, email, phone, address) VALUES('$f_id', '$firstname', '$lastname', '$email', '$phone', '$address')";
 //Prepare statement
       $stmt = sqlsrv_prepare($conn, $INSERT);
       sqlsrv_execute($stmt);
